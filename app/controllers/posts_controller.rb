1b02090adad041f73b73ee2bf8f66dfc1ee3d7c1
Matt <mbrowne108@gmail.com>
2022-02-24 12:23:22 -0500

    commit tree d0e7533bc7ce5dc1ed707ffd91db8900138316cf

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  rescue_from ActiveRecord::RecordInvalid, with: :render_not_found_response
 
   def show
     post = Post.find(params[:id])
@@ -9,8 +10,8 @@ class PostsController < ApplicationController
   def update
     post = Post.find(params[:id])
 
-    post.update(post_params)
-
+    post.update!(post_params)
+    
     render json: post
   end
 
@@ -18,6 +19,10 @@ class PostsController < ApplicationController
 
   def post_params
     params.permit(:category, :content, :title)
+  end
+
+  def render_not_found_response invalid
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
 end
